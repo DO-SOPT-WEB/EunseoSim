c34a82dc515d441f24713b13241a15c1f24b4f47
simeunseo <rkdcjfehowl@naver.com>
2023-10-27 05:17:55 +0900

    design: more btn styling

--- week2/assign1/script.js
+++ week2/assign1/script.js
@@ -4,3 +4,5 @@ const topBtn = document.getElementById("top-btn");
 document.addEventListener("scroll", () => {
   topBtn.style.opacity = window.scrollY / windowHeight / 2;
 });
+
+// const moreBtn = document.createElement("button");
