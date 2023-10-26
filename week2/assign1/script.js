adc3f9d15c8fe2f2c7d7662a057a10b88eaa8b7b
simeunseo <rkdcjfehowl@naver.com>
2023-10-27 05:27:07 +0900

    feat: add more btn depend on the length of text

--- week2/assign1/script.js
+++ week2/assign1/script.js
@@ -1,3 +1,5 @@
+/* 스크롤에 따른 topBtn 투명도 */
+
 const windowHeight = window.innerHeight;
 const topBtn = document.getElementById("top-btn");
 
@@ -5,4 +7,14 @@ document.addEventListener("scroll", () => {
   topBtn.style.opacity = window.scrollY / windowHeight / 2;
 });
 
-// const moreBtn = document.createElement("button");
+/* 글자수에 따른 더보기 버튼 활성화 및 작동 */
+const moreBtn = document.createElement("button"); //button 요소 생성
+moreBtn.innerText = "more";
+moreBtn.setAttribute("class", "content-more-btn"); //button 클래스명 지정
+
+const contents = document.querySelectorAll(".content");
+contents.forEach((content) => {
+  if (content.children[1].innerText.length >= 85) {
+    content.appendChild(moreBtn);
+  }
+});
