78727076bf9a5b93194a58c55b62e56afb95b9aa
simeunseo <rkdcjfehowl@naver.com>
2023-10-27 05:55:15 +0900

    feat: preview sectionscroll button

--- week2/assign1/script.js
+++ week2/assign1/script.js
@@ -7,10 +7,11 @@ document.addEventListener("scroll", () => {
   topBtn.style.opacity = window.scrollY / windowHeight / 2;
 });
 
+/* 글자수에 따른 더보기 버튼 활성화 및 작동 */
+
 const contents = document.querySelectorAll(".content");
 contents.forEach((content) => {
   const contentDescription = content.children[1];
-  /* 글자수에 따른 더보기 버튼 활성화 및 작동 */
   if (contentDescription.innerText.length >= 85) {
     const moreBtn = document.createElement("button"); //button 요소 생성
     moreBtn.innerText = "more";
@@ -24,4 +25,23 @@ contents.forEach((content) => {
       moreBtn.style.display = "none";
     });
   }
+});
+
+/* 미리보기 섹션 스크롤 버튼 */
+const scrollLeftBtn = document.getElementById("scroll-left-btn");
+const scrollRightBtn = document.getElementById("scroll-right-btn");
+
+const onboardingScroll = document.getElementById("onboarding-scroll");
+const maxScrollWidth = onboardingScroll.scrollWidth;
+
+scrollLeftBtn.addEventListener("click", () => {
+  onboardingScroll.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+});
+
+scrollRightBtn.addEventListener("click", () => {
+  onboardingScroll.scrollTo({
+    top: 0,
+    left: maxScrollWidth,
+    behavior: "smooth",
+  });
 });
