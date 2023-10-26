c592135402aa0ae972ae23a4f2d188a374e2210b
simeunseo <rkdcjfehowl@naver.com>
2023-10-27 05:03:55 +0900

    feat: ellipsis when text over 3 lines

--- week2/assign1/script.js
+++ week2/assign1/script.js
@@ -2,6 +2,5 @@ const windowHeight = window.innerHeight;
 const topBtn = document.getElementById("top-btn");
 
 document.addEventListener("scroll", () => {
-  console.log(window.scrollY, windowHeight);
   topBtn.style.opacity = window.scrollY / windowHeight / 2;
 });
