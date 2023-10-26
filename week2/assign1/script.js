f5a04908c8b039348b399f180c6f9fdcd0360dcf
simeunseo <rkdcjfehowl@naver.com>
2023-10-27 05:33:02 +0900

    feat: add more btn click event

--- week2/assign1/script.js
+++ week2/assign1/script.js
@@ -7,14 +7,21 @@ document.addEventListener("scroll", () => {
   topBtn.style.opacity = window.scrollY / windowHeight / 2;
 });
 
-/* 글자수에 따른 더보기 버튼 활성화 및 작동 */
-const moreBtn = document.createElement("button"); //button 요소 생성
-moreBtn.innerText = "more";
-moreBtn.setAttribute("class", "content-more-btn"); //button 클래스명 지정
-
 const contents = document.querySelectorAll(".content");
 contents.forEach((content) => {
-  if (content.children[1].innerText.length >= 85) {
-    content.appendChild(moreBtn);
+  const contentDescription = content.children[1];
+  /* 글자수에 따른 더보기 버튼 활성화 및 작동 */
+  if (contentDescription.innerText.length >= 85) {
+    const moreBtn = document.createElement("button"); //button 요소 생성
+    moreBtn.innerText = "more";
+    moreBtn.setAttribute("class", "content-more-btn"); //button 클래스명 지정
+
+    content.appendChild(moreBtn); //button 붙이기
+
+    /* 클릭 이벤트시 버튼 사라짐 */
+    content.addEventListener("click", () => {
+      contentDescription.style.display = "block"; //말줄임 표시를 없앰
+      moreBtn.style.display = "none";
+    });
   }
 });
