773d84749b0c9037b81fad4cdc6ee880d12815ff
simeunseo <rkdcjfehowl@naver.com>
2023-10-27 21:38:48 +0900

    fix: seperate modalhandler and modalworkhandler

--- week2/assign2/script.js
+++ week2/assign2/script.js
@@ -35,8 +35,8 @@ window.onload = () => {
   renderList(listData);
   countBalance(listData);
   handleFilterBtnClick(listData);
-  handleItemDelete(listData);
-  handleItemAdd(listData);
+  handleItemDeleteModal(listData);
+  handleItemAddModal(listData);
 };
 
 /* 계산된 나의 자산과 총 수입, 지출을 렌더링하는 함수 */
@@ -109,49 +109,74 @@ function handleFilterBtnClick(list) {
   renderList(filteredList);
 }
 
-/* 리스트 아이템 삭제를 동작시키는 함수 */
-function handleItemDelete(list) {
+/* 리스트 아이템 삭제 모달을 열고 닫는 함수 */
+function handleItemDeleteModal(list) {
+  console.log("handleItemDeleteModal작동");
   const deleteModal = document.getElementById("delete-btn-modal"); //모달
   const deleteModalConfirmBtn = document.getElementById("delete-btn-confirm"); //모달 확인 버튼
   const deleteModalCancelBtn = document.getElementById("delete-btn-cancel"); //모달 취소 버튼
 
   const deleteBtns = document.querySelectorAll(".delete-btn");
+
   deleteBtns.forEach((btn) => {
     btn.addEventListener("click", (e) => {
       let targetElement = e.target.parentElement;
 
       deleteModal.style.display = "flex";
+
       deleteModalCancelBtn.addEventListener("click", () => {
         deleteModal.style.display = "none";
-        targetElement = undefined;
+        targetElement = null;
       });
 
       deleteModalConfirmBtn.addEventListener("click", () => {
         deleteModal.style.display = "none";
 
-        if (targetElement) {
-          targetElement && targetElement.remove(); //화면(DOM)상에서 아이템 삭제
-
-          const targetId = parseInt(targetElement.id); //지워지는 아이템의 id
-          list = list.filter((item) => item.customId != targetId); //리스트에서 삭제한 아이템을 지움
-
-          localStorage.setItem("list_data", JSON.stringify(list)); //localStorage에 반영
-
-          countBalance(list); //나의 자산, 총 수입/지출에 반영
-        }
+        targetElement && handleItemDelete(list, targetElement);
       });
     });
   });
 }
 
-/* 리스트 아이템 등록을 동작시키는 함수 */
-function handleItemAdd(list) {
+/* 리스트 아이템 삭제를 동작시키는 함수 */
+function handleItemDelete(list, targetElement) {
+  targetElement && targetElement.remove(); //화면(DOM)상에서 아이템 삭제
+
+  const targetId = parseInt(targetElement.id); //지워지는 아이템의 id
+  list = list.filter((item) => item.customId != targetId); //리스트에서 삭제한 아이템을 지움
+
+  localStorage.setItem("list_data", JSON.stringify(list)); //localStorage에 반영
+  console.log("삭제한거 로컬에 반영함");
+
+  countBalance(list); //나의 자산, 총 수입/지출에 반영
+}
+
+/* 리스트 아이템 등록 모달을 열고 닫는 함수 */
+function handleItemAddModal(list) {
+  const addBtn = document.getElementById("add-btn"); //아이템 등록 버튼
+  const addModal = document.getElementById("add-btn-modal"); //모달
+
+  const addModalCancelBtn = document.getElementById("add-form-cancel-btn"); //모달 취소 버튼
+
   const addFormRadioIncome = document.getElementById("add-form-radio-income"); //수입 radio 버튼
   const addFormRadioSpending = document.getElementById(
     "add-form-radio-spending" //지출 radio 버튼
   );
-
   const addFormSelect = document.getElementById("add-form-category-select"); //select box
+  const costInput = document.getElementById("add-form-cost-input");
+  const nameInput = document.getElementById("add-form-name-input");
+
+  addBtn.addEventListener("click", () => {
+    addModal.style.display = "flex";
+  });
+
+  addModalCancelBtn.addEventListener("click", () => {
+    addModal.style.display = "none";
+    addFormRadioIncome.checked = true;
+    addFormRadioSpending.checked = false;
+    costInput.value = "";
+    nameInput.value = "";
+  });
 
   handleSelectOption(); //최초 설정
   addFormRadioIncome.addEventListener("click", handleSelectOption);
@@ -172,21 +197,7 @@ function handleItemAdd(list) {
     });
   }
 
-  const addBtn = document.getElementById("add-btn"); //아이템 등록 버튼
-  const addModal = document.getElementById("add-btn-modal"); //모달
   const addModalConfirmBtn = document.getElementById("add-form-submit-btn"); //모달 확인 버튼
-  const addModalCancelBtn = document.getElementById("add-form-cancel-btn"); //모달 취소 버튼
-
-  addBtn.addEventListener("click", () => {
-    addModal.style.display = "flex";
-  });
-
-  addModalCancelBtn.addEventListener("click", () => {
-    addModal.style.display = "none";
-  });
-
-  const costInput = document.getElementById("add-form-cost-input");
-  const nameInput = document.getElementById("add-form-name-input");
 
   costInput.addEventListener("keyup", (e) => {
     let value = e.target.value;
@@ -201,10 +212,42 @@ function handleItemAdd(list) {
 
   addModalConfirmBtn.addEventListener("click", (e) => {
     e.preventDefault();
-    if (!addFormSelect.value || !costInput.value || !nameInput.value) {
-      alert("모든 항목을 입력해 주세요.");
-    } else if (isNaN(costInput.value)) {
-      alert("금액은 숫자로 입력해 주세요.");
-    }
+    handleItemAdd(
+      list,
+      addFormSelect,
+      costInput,
+      nameInput,
+      addFormRadioIncome
+    );
   });
+}
+
+/* 리스트 아이템 등록을 동작시키는 함수 */
+function handleItemAdd(
+  list,
+  addFormSelect,
+  costInput,
+  nameInput,
+  addFormRadioIncome
+) {
+  if (!addFormSelect.value || !costInput.value || !nameInput.value) {
+    alert("모든 항목을 입력해 주세요.");
+  } else {
+    //validation 통과
+    const newItem = {
+      customId: list[list.length - 1].customId + 1, //현재 리스트의 마지막 요소의 customId의 다음 번호로 새로운 customId를 정함
+      category: addFormSelect.value,
+      name: nameInput.value,
+      cost: addFormRadioIncome.checked
+        ? Number(costInput.value.replaceAll(",", ""))
+        : -1 * Number(costInput.value.replaceAll(",", "")),
+    };
+    console.log("등록하다");
+    list.push(newItem);
+    localStorage.setItem("list_data", JSON.stringify(list));
+
+    countBalance(list);
+    renderList(list);
+  }
+  handleItemDeleteModal(list);
 }
