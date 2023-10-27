59c9173c2c2eabedf102df859d396f9cb29ccbbe
simeunseo <rkdcjfehowl@naver.com>
2023-10-27 16:27:11 +0900

    docs: add constant datas

---
+++ week2/assign2/data.js
@@ -0,0 +1,23 @@
+const INIT_BALANCE = 0;
+const HISTORY_LIST = [
+  {
+    category: "식비",
+    name: "크라이치즈버거 역곡점",
+    cost: -10800,
+  },
+  {
+    category: "취미",
+    name: "포토그레이 부천점",
+    cost: -4000,
+  },
+  {
+    category: "월급",
+    name: "근로장학",
+    cost: 300000,
+  },
+  {
+    category: "쇼핑",
+    name: "풋락커 커먼그라운드점",
+    cost: -99000,
+  },
+];
