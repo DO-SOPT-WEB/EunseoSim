c7f04192bbb3835edda34cff4a107497ddf355ba
simeunseo <rkdcjfehowl@naver.com>
2023-10-27 20:03:25 +0900

    feat: add form select option dynamically

--- week2/assign2/script.js
+++ week2/assign2/script.js
@@ -1,7 +1,11 @@
+import { INCOME_CATEGORY } from "./data.js";
 import { INIT_BALANCE } from "./data.js";
 import { LIST_DATA } from "./data.js";
+import { SPENDING_CATEGORY } from "./data.js";
 
-let listData = []; //localStorage에 저장된 목록을 가져와 저장하는 배열
+let listData = []; //localStorage에 저장된 가계무 목록을 가져와 저장하는 배열
+let incomeCategoryData = []; //localStorage에 저장된 수입 카테고리 목록을 가져와 저장하는 배열
+let spendingCategoryData = []; //localStorage에 저장된 지출 카테고리 목록을 가져와 저장하는 배열
 let balance = INIT_BALANCE; //나의 총 자산
 let income = INIT_BALANCE; //총 수입
 let spending = INIT_BALANCE; //총 지출
@@ -10,9 +14,24 @@ window.onload = () => {
   //localStorage 초기화
   localStorage.getItem("list_data") === null &&
     localStorage.setItem("list_data", JSON.stringify(LIST_DATA));
+  localStorage.getItem("income_category_data") === null &&
+    localStorage.setItem(
+      "income_category_data",
+      JSON.stringify(INCOME_CATEGORY)
+    );
+  localStorage.getItem("spending_category_data") === null &&
+    localStorage.setItem(
+      "spending_category_data",
+      JSON.stringify(SPENDING_CATEGORY)
+    );
 
   //localStorage에 저장된 목록을 가져옴
   listData = JSON.parse(localStorage.getItem("list_data"));
+  incomeCategoryData = JSON.parse(localStorage.getItem("income_category_data"));
+  spendingCategoryData = JSON.parse(
+    localStorage.getItem("spending_category_data")
+  );
+
   renderList(listData);
   countBalance(listData);
   handleFilterBtnClick(listData);
@@ -125,7 +144,33 @@ function handleItemDelete(list) {
   });
 }
 
+/* 리스트 아이템 등록을 동작시키는 함수 */
 function handleItemAdd(list) {
+  const addFormRadioIncome = document.getElementById("add-form-radio-income");
+  const addFormRadioSpending = document.getElementById(
+    "add-form-radio-spending"
+  );
+
+  const addFormSelect = document.getElementById("add-form-category-select");
+
+  handleSelectOption();
+  addFormRadioIncome.addEventListener("click", handleSelectOption);
+  addFormRadioSpending.addEventListener("click", handleSelectOption);
+
+  function handleSelectOption() {
+    const currentOption = addFormRadioIncome.checked
+      ? incomeCategoryData
+      : spendingCategoryData;
+
+    addFormSelect.replaceChildren();
+    currentOption.forEach((category) => {
+      const option = document.createElement("option");
+      option.text = category;
+      option.value = category;
+      addFormSelect.add(option);
+    });
+  }
+
   const addBtn = document.getElementById("add-btn"); //아이템 등록 버튼
   const addModal = document.getElementById("add-btn-modal"); //모달
   const deleteModalConfirmBtn = document.getElementById("add-form-submit-btn"); //모달 확인 버튼
