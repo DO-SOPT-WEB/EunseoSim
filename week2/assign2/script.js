4c18f827b08a7ba2b63fe310cf249cc22f2b4cd2
simeunseo <rkdcjfehowl@naver.com>
2023-10-27 17:47:44 +0900

    feat: income and spending button filtering

--- week2/assign2/script.js
+++ week2/assign2/script.js
@@ -2,9 +2,6 @@ import { INIT_BALANCE } from "./data.js";
 import { LIST_DATA } from "./data.js";
 
 let listData = []; //localStorage에 저장된 목록을 가져와 저장하는 배열
-let balance = INIT_BALANCE; //나의 총 자산
-let income = INIT_BALANCE; //총 수입
-let spending = INIT_BALANCE; //총 지출
 
 window.onload = () => {
   //localStorage 초기화
@@ -14,10 +11,21 @@ window.onload = () => {
   //localStorage에 저장된 목록을 가져옴
   listData = JSON.parse(localStorage.getItem("list_data"));
   renderList(listData);
+  countBalance(listData);
+  handleFilterBtnClick(listData);
 };
 
 /* 계산된 나의 자산과 총 수입, 지출을 렌더링하는 함수 */
-function countBalance() {
+function countBalance(list) {
+  let balance = INIT_BALANCE; //나의 총 자산
+  let income = INIT_BALANCE; //총 수입
+  let spending = INIT_BALANCE; //총 지출
+
+  list.forEach((item) => {
+    balance += item.cost;
+    item.cost < 0 ? (spending += item.cost) : (income += item.cost);
+  });
+
   const balanceNode = document.getElementById("balance");
   balanceNode.innerText = balance.toLocaleString() + "원";
 
@@ -33,10 +41,8 @@ function renderList(list) {
   const listWrapper = document.getElementById("list-wrapper"); //리스트가 들어갈 부모 노드
   const itemTemplate = document.getElementById("item-template"); //하나의 가계부 내역 아이템 템플릿
 
+  listWrapper.replaceChildren();
   list.forEach((item) => {
-    balance += item.cost;
-    item.cost < 0 ? (spending += item.cost) : (income += item.cost);
-
     let itemContent = itemTemplate.cloneNode(true); //템플릿 복사
     let itemNewHtml = itemContent.innerHTML; //템플릿 안의 html 복사
 
@@ -49,5 +55,31 @@ function renderList(list) {
     itemContent.innerHTML = itemNewHtml; //새롭게 바뀐 html을 템플릿에 적용
     listWrapper.appendChild(itemContent.content); //부모노드 안에 넣기
   });
-  countBalance();
+}
+
+/* 수입/지출 버튼 클릭을 통한 필터링을 동작시키는 함수 */
+function handleFilterBtnClick(list) {
+  const incomeBtn = document.getElementById("checkbox-income");
+  const spendingBtn = document.getElementById("checkbox-spending");
+
+  let filteredList = list;
+
+  function handleFiltering() {
+    if (incomeBtn.checked && !spendingBtn.checked) {
+      filteredList = list.filter((item) => item.cost >= 0);
+      renderList(filteredList);
+    } else if (!incomeBtn.checked && spendingBtn.checked) {
+      filteredList = list.filter((item) => item.cost < 0);
+      renderList(filteredList);
+    } else if (!incomeBtn.checked && !spendingBtn.checked) {
+      renderList([]);
+    } else {
+      renderList(list);
+    }
+  }
+
+  incomeBtn.addEventListener("click", handleFiltering);
+  spendingBtn.addEventListener("click", handleFiltering);
+
+  renderList(filteredList);
 }
