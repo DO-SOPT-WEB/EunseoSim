d5a96e85889bbe3cb610d5c8cc70764e378cf0db
simeunseo <rkdcjfehowl@naver.com>
2023-10-27 18:21:17 +0900

    feat: delete item

--- week2/assign2/data.js
+++ week2/assign2/data.js
@@ -1,21 +1,25 @@
 export const INIT_BALANCE = 0;
 export const LIST_DATA = [
   {
+    customId: 123,
     category: "식비",
     name: "크라이치즈버거 역곡점",
     cost: -10800,
   },
   {
+    customId: 124,
     category: "취미",
     name: "포토그레이 부천점",
     cost: -4000,
   },
   {
+    customId: 125,
     category: "월급",
     name: "근로장학",
     cost: 300000,
   },
   {
+    customId: 126,
     category: "쇼핑",
     name: "풋락커 커먼그라운드점",
     cost: -99000,
--- week2/assign2/script.js
+++ week2/assign2/script.js
@@ -2,6 +2,9 @@ import { INIT_BALANCE } from "./data.js";
 import { LIST_DATA } from "./data.js";
 
 let listData = []; //localStorage에 저장된 목록을 가져와 저장하는 배열
+let balance = INIT_BALANCE; //나의 총 자산
+let income = INIT_BALANCE; //총 수입
+let spending = INIT_BALANCE; //총 지출
 
 window.onload = () => {
   //localStorage 초기화
@@ -13,14 +16,11 @@ window.onload = () => {
   renderList(listData);
   countBalance(listData);
   handleFilterBtnClick(listData);
+  handleItemDelete(listData);
 };
 
 /* 계산된 나의 자산과 총 수입, 지출을 렌더링하는 함수 */
 function countBalance(list) {
-  let balance = INIT_BALANCE; //나의 총 자산
-  let income = INIT_BALANCE; //총 수입
-  let spending = INIT_BALANCE; //총 지출
-
   list.forEach((item) => {
     balance += item.cost;
     item.cost < 0 ? (spending += item.cost) : (income += item.cost);
@@ -41,7 +41,7 @@ function renderList(list) {
   const listWrapper = document.getElementById("list-wrapper"); //리스트가 들어갈 부모 노드
   const itemTemplate = document.getElementById("item-template"); //하나의 가계부 내역 아이템 템플릿
 
-  listWrapper.replaceChildren();
+  listWrapper.replaceChildren(); //초기화
   list.forEach((item) => {
     let itemContent = itemTemplate.cloneNode(true); //템플릿 복사
     let itemNewHtml = itemContent.innerHTML; //템플릿 안의 html 복사
@@ -50,7 +50,8 @@ function renderList(list) {
       .replace("{item-category}", item.category)
       .replace("{item-name}", item.name)
       .replace("{item-cost}", item.cost.toLocaleString())
-      .replace("{item-type}", item.cost < 0 ? "spending" : "income");
+      .replace("{item-type}", item.cost < 0 ? "spending" : "income")
+      .replace("{item-id}", item.customId);
 
     itemContent.innerHTML = itemNewHtml; //새롭게 바뀐 html을 템플릿에 적용
     listWrapper.appendChild(itemContent.content); //부모노드 안에 넣기
@@ -82,4 +83,19 @@ function handleFilterBtnClick(list) {
   spendingBtn.addEventListener("click", handleFiltering);
 
   renderList(filteredList);
+}
+
+/* 리스트 아이템 삭제를 동작시키는 함수 */
+function handleItemDelete(list) {
+  const deleteBtns = document.querySelectorAll(".delete-btn");
+  deleteBtns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      e.target.parentElement.remove(); //화면(DOM)상에서 아이템 삭제
+
+      const targetId = parseInt(e.target.parentElement.id); //지워지는 아이템의 id
+      list = list.filter((item) => item.customId != targetId); //리스트에서 삭제한 아이템을 지움
+
+      localStorage.setItem("list_data", JSON.stringify(list)); //localStorage에 반영
+    });
+  });
 }
